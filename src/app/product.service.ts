e2335b5448959c874c1312bd9b58be155d8d1a70
Erik Sylvan <erik.sylvan@delaval.com>
2019-03-27 22:25:56 +0100

    Added Product list

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -4,12 +4,14 @@ import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map'
 
 import { Album } from './album'
+import { Product } from './product'
 import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class ProductService {
   
   private _albumUrl = '../assets/album.json'
+  private _productsUrl = '../assets/products.json'
   
   constructor(private _http: Http) { 
   
@@ -18,4 +20,9 @@ export class ProductService {
   getAlbum(id: number):Observable<Album>{
     return this._http.get(this._albumUrl).map((response) => <Album>response.json());
   }
+
+  getProducts():Observable<Product[]>{
+    return this._http.get(this._productsUrl).map((response) => <Product[]>response.json());
+  }
+
 }
