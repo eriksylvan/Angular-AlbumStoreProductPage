37fefc442de2ee8b75b2be866cd62695953f57c6
Erik Sylvan <erik.sylvan@delaval.com>
2019-03-27 20:10:31 +0100

    fix Product Service

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -6,7 +6,7 @@ import 'rxjs/add/operator/map'
 @Injectable()
 export class ProductService {
   
-  _albumUrl = '../assets/album.json'
+  private _albumUrl = '../assets/album.json'
   
   constructor(private _http: Http) { 
   
